9af16050974ad765c6716ab2875dc858ba67b31b
Ezequiel Etchecoin <eze_etchecoin_92@hotmail.com>
2020-04-25 11:26:50 -0300

    comenzamos modulo accuweather weather data retriever

---
+++ accuweather-weather-data-retriever/accuweather-retriever.js
@@ -0,0 +1,12 @@
+//Modify this with ApiKey to use
+const apiKey = 'TzVuWmyxAeJzUJOuixA7LHnGDooYk4lE';
+
+async function getLocationKeyByLocationName (locationName) {
+    
+}
+
+async function getCurrentWeatherByLocationKey (locationKey){
+
+}
+
+module.exports = { getLocationKeyByLocationName, getCurrentWeatherByLocationKey };
